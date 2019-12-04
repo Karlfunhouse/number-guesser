57b00de1e20718c64e396e088b37e6ffba7c4d0d
kmbarnett <kmbarnett94@gmail.com>
2019-12-04 13:24:16 -0700

    Change implemented changes requested in PR

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 (function() {
   var clearButton = document.querySelector('#clear-button')
-  var input = document.querySelectorAll('.game-input')
+  var challengerInputField = document.querySelectorAll('.game-input')
 
   function enableButton() {
     clearButton.removeAttribute('disabled')
     }
 
   function clearFields() {
-    for (var i = 0; i < input.length; i++) {
-      input[i].value = " "
+    for (var i = 0; i < challengerInputField.length; i++) {
+      challengerInputField[i].value = " "
     }
     clearButton.setAttribute('disabled', true)
   }
 
-  input[0].addEventListener('input', enableButton)
-  input[1].addEventListener('input', enableButton)
-  input[2].addEventListener('input', enableButton)
-  input[3].addEventListener('input', enableButton)
+  challengerInputField[0].addEventListener('input', enableButton)
+  challengerInputField[1].addEventListener('input', enableButton)
+  challengerInputField[2].addEventListener('input', enableButton)
+  challengerInputField[3].addEventListener('input', enableButton)
   clearButton.addEventListener('click', clearFields)
 }());
